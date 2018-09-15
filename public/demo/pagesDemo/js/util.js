6d4779b22c0ad63649dca6bf8af1a482f0abfdc8
brizer <362512489@qq.com>
2018-09-15 14:12:01 +0800

    @dev promise thenable demo

--- public/demo/pagesDemo/js/util.js
+++ public/demo/pagesDemo/js/util.js
@@ -1,6 +1,9 @@
 var Util = (function(){
     var dw = function(_info){
-        document.write(_info+'</br>')
+        var div = document.createElement('div');
+        div.innerHTML = _info + '</br>';
+        document.body.appendChild(div);
+        //document.write(_info+'</br>')
     }
     return {
         dw:dw
